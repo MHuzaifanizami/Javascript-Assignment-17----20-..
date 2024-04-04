030880896e0693c2f5fe14b5c824ab00c92efea0
M Huzaifa Nizami <153551455+MHuzaifanizami@users.noreply.github.com>
2024-04-05 00:00:13 +0500

    file upload

---
+++ app.js
@@ -0,0 +1,124 @@
+
+// Assignment # 17-20
+
+// Q no 01 
+
+// var arr = [[], [] , [] , []];
+
+// Q no 02 
+
+// var arr = [[0,1,2,3] , [1,0,1,2] ,[2,1,0,1]];
+// for(var i = 0 ; i < arr.length ; i++){
+// document.write(arr[i] + "<br>")
+// }
+
+
+//////////////////////////// Q no 03 
+
+
+
+// var i = 0;
+// for(var i = 1 ; i < 11 ; i++){
+
+//     document.write(i + "<br>")
+// }
+
+
+
+
+// var userNumber = prompt('Enter Your Number');
+// var userLength = prompt('Enter Your Length');
+
+// for(userNumber = 0 ; i = userNumber ; i = i + 1){
+
+//     document.write(i)
+
+// }
+
+// Q no 04 
+
+
+// var userTable = prompt('Enter Your Table Number');
+// var userLength = prompt('Enter Your Table Length');
+
+// for(var i = 1 ; i < userLength ; i++ ){
+
+//     document.write(userTable + "x" + i+ "=" + userTable *i )
+// }
+
+// Q NO 05 
+
+// var fruits = ['apple' , 'mango' , 'banana' , 'orange'];
+
+// for(var i = 0 ; i < fruits.length ; i++){
+
+//     document.write(fruits[i] +'<br>');
+
+// }
+// for(var i = 0 ; i < fruits.length ; i++){
+
+//     document.write('Element at index'+" "+ i +" "+ fruits[i] +'<br>');
+
+// }
+
+
+// Q no 06 (a)
+// document.write(`<h1>Counting</h1>`)
+// for(i=1 ; i < 16 ; i++){
+
+//     document.write(` ${i} `)
+// }
+
+// Q no 06 (b)
+
+// for(i=10 ; i > 0; i--){
+    
+//     document.write(` ${i} `)
+// }
+
+// Q no 06 (c)
+
+// for(i=0 ; i < 20 ; i = i+2 ){
+
+//     document.write(i)
+// }
+
+// Q no 6 (d)
+
+// for(i=1 ; i <20 ; i= i+2 ){
+//     document.write(i + " " )
+// }
+
+// Q no 6 (e)
+
+// for(i=2 ; i <21 ; i = i+2){
+//     document.write(i+"k"+" ")
+// }
+
+
+// Q no 07 
+// var userinpt = prompt('What do you want to order')
+
+// var bakery = ['cake' , 'pestry' , 'biscuit' , 'chips' , 'petties'];
+
+
+// for(i=0 ; i<bakery.length ;i++){
+//     var ismatch = false
+//  if(userinpt === bakery[i] ){
+//     alert( userinpt +  " is available at index of " + i)
+//     ismatch = true
+//     break
+//  }
+//  }
+//  if(ismatch == false){
+//     alert("cokkies is not available ")
+
+//  }
+
+
+// Q no 10 
+
+// for(i=5 ; i<101 ; i = i+5){
+//     document.write(i+ " ")
+// }
+
